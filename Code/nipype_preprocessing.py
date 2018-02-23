e84be961275bfd1dd4bd159bd7cd382ba5279f41
SaraZM <sara.zapata-marin@mail.mcgill.ca>
2018-02-23 13:25:05 -0500

    Preprocessing nipype tutorial with some errors in the files selection part

--- Code/nipype_preprocessing.py
+++ Code/nipype_preprocessing.py
@@ -19,7 +19,7 @@ subject_list = ['sub-1', 'sub-2', 'sub-3', 'sub-4', 'sub-5', 'sub-6']
 fwhm=[4,8]
 # full width at half maximum
 #filter width in space
-#task_list=['func']
+task_list=['objectviewing']
 
 
 with open('ds000105/task-objectviewing_bold.json', 'rt') as fp:
@@ -95,36 +95,100 @@ coregwf = Workflow(name='coregwf')
 coregwf.base_dir = opj(experiment_dir, working_dir)
 #
 ## Connect all components of the coregistration workflow
-#coregwf.connect([(bet_anat, segmentation, [('out_file', 'in_files')]),
-#                 (segmentation, threshold, [(('partial_volume_files', get_wm),
-#                                             'in_file')]),
-#                 (bet_anat, coreg_pre, [('out_file', 'reference')]),
-#                 (threshold, coreg_bbr, [('out_file', 'wm_seg')]),
-#                 (coreg_pre, coreg_bbr, [('out_matrix_file', 'in_matrix_file')]),
-#                 (coreg_bbr, applywarp, [('out_matrix_file', 'in_matrix_file')]),
-#                 (bet_anat, applywarp, [('out_file', 'reference')]),
-#                 (coreg_bbr, applywarp_mean, [('out_matrix_file', 'in_matrix_file')]),
-#                 (bet_anat, applywarp_mean, [('out_file', 'reference')]),
-#                 ])
+coregwf.connect([(bet_anat, segmentation, [('out_file', 'in_files')]),
+                 (segmentation, threshold, [(('partial_volume_files', get_wm),
+                                             'in_file')]),
+                 (bet_anat, coreg_pre, [('out_file', 'reference')]),
+                 (threshold, coreg_bbr, [('out_file', 'wm_seg')]),
+                 (coreg_pre, coreg_bbr, [('out_matrix_file', 'in_matrix_file')]),
+                 (coreg_bbr, applywarp, [('out_matrix_file', 'in_matrix_file')]),
+                 (bet_anat, applywarp, [('out_file', 'reference')]),
+                 (coreg_bbr, applywarp_mean, [('out_matrix_file', 'in_matrix_file')]),
+                 (bet_anat, applywarp_mean, [('out_file', 'reference')]),
+                 ])
+# Infosource - a function free node to iterate over the list of subject names
+infosource = Node(IdentityInterface(fields=['subject_id', 'task_name']),
+                  name="infosource")
+infosource.iterables = [('subject_id', subject_list),
+                        ('task_name', task_list)]
+
+# SelectFiles - to grab the data (alternativ to DataGrabber)
+anat_file = opj('derivatives', 'fmriprep', '{subject_id}', 'anat', '{subject_id}_T1w.nii.gz')
+func_file = opj('{subject_id}', 'ses-test', 'func',
+                '{subject_id}_task-{task_name}_run-01_bold.nii.gz')
+
+templates = {'anat': anat_file,
+             'func': func_file}
+selectfiles = Node(SelectFiles(templates,
+                               base_directory='ds000105/'),
+                   name="selectfiles")
+
+# Datasink - creates output folder for important outputs
+datasink = Node(DataSink(base_directory=experiment_dir,
+                         container=output_dir),
+                name="datasink")
+
+## Use the following DataSink output substitutions
+substitutions = [('_subject_id_', ''),
+                 ('_task_name_', '/task-'),
+                 ('_fwhm_', 'fwhm-'),
+                 ('_roi', ''),
+                 ('_mcf', ''),
+                 ('_st', ''),
+                 ('_flirt', ''),
+                 ('.nii_mean_reg', '_mean'),
+                 ('.nii.par', '.par'),
+                 ]
+subjFolders = [('fwhm-%s/' % f, 'fwhm-%s_' % f) for f in fwhm]
+substitutions.extend(subjFolders)
+datasink.inputs.substitutions = substitutions
+#
+# Create a preprocessing workflow
+preproc = Workflow(name='preproc')
+preproc.base_dir = opj(experiment_dir, working_dir)
+
+# Connect all components of the preprocessing workflow
+preproc.connect([(infosource, selectfiles, [('subject_id', 'subject_id'),
+                                            ('task_name', 'task_name')]),
+                 (selectfiles, extract, [('func', 'in_file')]),
+                 (extract, mcflirt, [('roi_file', 'in_file')]),
+                 (mcflirt, slicetimer, [('out_file', 'in_file')]),
+
+                 (selectfiles, coregwf, [('anat', 'bet_anat.in_file'),
+                                         ('anat', 'coreg_bbr.reference')]),
+                 (mcflirt, coregwf, [('mean_img', 'coreg_pre.in_file'),
+                                     ('mean_img', 'coreg_bbr.in_file'),
+                                     ('mean_img', 'applywarp_mean.in_file')]),
+                 (slicetimer, coregwf, [('slice_time_corrected_file', 'applywarp.in_file')]),
+                 
+                 (coregwf, smooth, [('applywarp.out_file', 'in_files')]),
+
+                 (mcflirt, datasink, [('par_file', 'preproc.@par')]),
+                 (smooth, datasink, [('smoothed_files', 'preproc.@smooth')]),
+                 (coregwf, datasink, [('applywarp_mean.out_file', 'preproc.@mean')]),
+
+                 (coregwf, art, [('applywarp.out_file', 'realigned_files')]),
+                 (mcflirt, art, [('par_file', 'realignment_parameters')]),
+
+                 (coregwf, datasink, [('coreg_bbr.out_matrix_file', 'preproc.@mat_file'),
+                                      ('bet_anat.out_file', 'preproc.@brain')]),
+                 (art, datasink, [('outlier_files', 'preproc.@outlier_files'),
+                                  ('plot_files', 'preproc.@plot_files')]),
+                 ])
+
+# Create preproc output graph
+preproc.write_graph(graph2use='colored', format='png', simple_form=True)
+
+# Visualize the graph
+from IPython.display import Image
+Image(filename=opj(preproc.base_dir, 'preproc', 'graph.dot.png'))
+## Visualize the detailed graph
+preproc.write_graph(graph2use='flat', format='png', simple_form=True)
+Image(filename=opj(preproc.base_dir, 'preproc', 'graph_detailed.dot.png'))
+##
+##
 #
 #
 #
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+
