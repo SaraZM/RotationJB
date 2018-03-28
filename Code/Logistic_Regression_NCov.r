bf993c83739259ec00052cac1923dc6cd567558c
SaraZM <sara.zapata-marin@mail.mcgill.ca>
2018-03-28 13:30:48 -0400

    Optimizing logaritmo function, change name of variables and comments added

--- Code/Logistic_Regression_NCov.r
+++ Code/Logistic_Regression_NCov.r
@@ -1,43 +1,43 @@
 n<-100
-#x<-rnorm(n,0,1)
+# Define the true value of beta0
 beta0<-0.1
-#beta1<-1.2
-mean<-0
-var <- 10
-#+beta1*x
-prob<-exp(mean)/(1+exp(mean))
-y<-rbinom(n,1,prob)
+# Mean and variance of beta0 beta0 ~ N(m0,c0)
+m0<-0
+v0 <- 10
+pi_i <-exp(mean)/(1+exp(mean))
+# Simulate data for y as a random Bernoulli distribution
+y<-rbinom(n,1,pi_i)
 iter=2000
-chain_beta =matrix(0, ncol=iter)
-chain_beta[1]=1
+chain_beta0 =matrix(0, ncol=iter)
+chain_beta0[1]=1
 
 
-### Define function for beta_c and beta_p
+### Define function for the numerator and denominator
 logaritmo <- function(beta){
-  likel<-0
-  for (i in 1:n){
-    logs <- y[i]*(beta-log(1+exp(beta)))-(1-y[i])*log(1+exp(beta))
-    likel <- logs+likel
-  }
-  fbeta <- likel-0.5*(1/var)*(beta-mean)^2
+  likel <- y*(beta-log(1+exp(beta)))-(1-y)*log(1+exp(beta))
+  fbeta <- sum(likel)-0.5*(1/var)*(beta-mean)^2
   return(fbeta)
 }
-
-#b0 ~ dnorm(0, sd=100)
+# Define the variance of the proposed beta0
 u=0.01
 #########
 for(i in 2:iter){
-  beta_c=chain_beta[i-1]
-  beta_p=rnorm(1, beta_c, sqrt(u))
-  #beta_p=exp(log_beta_p)
-  
-  log_ratio_beta =logaritmo(beta_p)-logaritmo(beta_c)
+# Current beta0 takes the first value in the chain
+  beta0_c=chain_beta0[i-1]
+# Proposed beta0 comes from a random normal distribution
+  beta0_p=rnorm(1, beta0_c, sqrt(u))
+# Calculate the ratio using the function logaritmo evaluated in proposed beta0 and current beta0
+  log_ratio_beta =logaritmo(beta0_p)-logaritmo(beta0_c)
+# Metropolis-Hastings, toss a coin 
   coin=log(runif(1))
-
+# Accept proposed value with prob. = min (1, ratio)
   if(coin<min(c(0, log_ratio_beta))){
-    chain_beta[i]=beta_p
+    chain_beta0[i]=beta0_p
 
   }else{
-  chain_beta[i]=beta_c  
+  chain_beta0[i]=beta0_c  
   }
 }
+
+hist(chain_beta0[1,])
+abline(v=0.1)
